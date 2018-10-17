ec1e165c3cec0717a5c2e0d2ee4300bb24c1f561
TurtleFeeder <t.choong@yahoo.com>
2018-10-17 16:04:05 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  def song_count
+    songs.count
+    # same as self.songs.count
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,9 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    artist.name
+    # same as self.artist.name
+  end
+
 end
